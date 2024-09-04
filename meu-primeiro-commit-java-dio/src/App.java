16519ea15eeec8ce3960683359c305cfc13fd77e
bergamaschidev <rodrigo.bergamaschidev@gmail.com>
2024-09-03 21:28:53 -0300

    Alteração do texto em System.out

--- meu-primeiro-commit-java-dio/src/App.java
+++ meu-primeiro-commit-java-dio/src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Bem vindo Rodrigo, este é seu primeio projeto em java na Dio!");
     }
 }
